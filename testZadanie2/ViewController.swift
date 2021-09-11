6ffb3b01a49e09261dffc53d45fa84a50bc8195e
Andrey <nefedovandrey1997@gmail.com>
2021-09-11 23:21:20 +0300

    Initial Commit

---
+++ testZadanie2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testZadanie2
+//
+//  Created by Andrey on 11.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
